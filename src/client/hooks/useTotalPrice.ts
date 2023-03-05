85f4d9601a52a1275300ec34e3397fc902f73353
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2023-03-05 12:53:38 +0900

    何のtimerなんこれ

--- src/client/hooks/useTotalPrice.ts
+++ src/client/hooks/useTotalPrice.ts
@@ -7,22 +7,14 @@ export function useTotalPrice(order: OrderFragmentResponse) {
   const [totalPrice, setTotalPrice] = useState<number>(0);
 
   useEffect(() => {
-    let timer = (function tick() {
-      return setTimeout(() => {
-        let total = 0;
-        for (const item of order.items) {
-          const offer = getActiveOffer(item.product.offers);
-          const price = offer?.price ?? item.product.price;
-          total += price * item.amount;
-        }
-        setTotalPrice(total);
-        timer = tick();
-      }, 500);
-    })();
+    let total = 0;
+    for (const item of order.items) {
+      const offer = getActiveOffer(item.product.offers);
+      const price = offer?.price ?? item.product.price;
+      total += price * item.amount;
+    }
+    setTotalPrice(total);
 
-    return () => {
-      clearTimeout(timer);
-    };
   }, [order]);
 
   return { totalPrice };
