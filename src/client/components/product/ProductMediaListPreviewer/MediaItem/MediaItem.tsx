c27769c574c28313d6066cf485374576f3fb6c91
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2023-03-06 16:37:03 +0900

    動画のサムネ追加した

--- src/client/components/product/ProductMediaListPreviewer/MediaItem/MediaItem.tsx
+++ src/client/components/product/ProductMediaListPreviewer/MediaItem/MediaItem.tsx
@@ -7,21 +7,21 @@ import { Icon } from '../../../foundation/Icon';
 import { Image } from '../../../foundation/Image';
 
 import * as styles from './MediaItem.styles';
-import { loadThumbnail } from './loadThumbnail';
 
 type Props = {
   file: MediaFileFragmentResponse;
 };
+const REG = /[0-9]{3}/
 
 export const MediaItem: FC<Props> = ({ file }) => {
   const [imageSrc, setImageSrc] = useState<string>();
   const mediaType = getMediaType(file.filename);
-
   useEffect(() => {
     if (mediaType === 'image') {
       return setImageSrc(file.filename);
     }
-    loadThumbnail(file.filename).then((url) => setImageSrc(url));
+    const videoNumber = file.filename.match(REG)
+    setImageSrc(`/images/thumbnail/${videoNumber}.webp`)
   }, [file.filename, mediaType]);
 
   if (imageSrc === undefined) {
